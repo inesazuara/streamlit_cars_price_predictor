e94bbbcbf12c012ea9b63563ad20151a775f78c1
Inés <ines.azuarah@gmail.com>
2024-10-06 18:36:06 +0200

    Developed and saved model code

--- model.py
+++ model.py
@@ -1,4 +1,11 @@
+# Importing libraries
 import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn import ensemble
+from sklearn.metrics import root_mean_squared_error
+import numpy as np
+
 
 # Loading dataset for training
 def load_data(file_path,sep):
@@ -30,12 +37,60 @@ def preprocess_data(df):
                            drop_first=True,
                            columns=['num_owners'])
 
+       # Assigning median to the rest of the columns with null values
+       for col in ['months_old', 'power', 'kms']:
+           df[col] = df[col].fillna(df[col].median())
 
        return df
+
+
+def train_model(data):
+    X = data.drop('price',axis=1)
+    y = data['price']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    model = ensemble.AdaBoostRegressor()
+
+    # Train the model
+    model.fit(X_train, y_train)
+    return model, X_test, X_train, y_test, y_train
+
+def evaluate_model(model, X_train, X_test, y_train, y_test):
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+
+    # Calculating RMSE for train and test
+    rmse_train = round(root_mean_squared_error(y_train, y_train_pred), 2)
+    rmse_test = round(root_mean_squared_error(y_test, y_test_pred), 2)
+
+    # Degradation
+    degradation = round((rmse_test - rmse_train)/rmse_train*100,2)
+
+    # Printing results
+    print(f"RMSE (Train): {rmse_train}")
+    print(f"RMSE (Test): {rmse_test}")
+    print(f"Degradation: {degradation}%")
+
+    return rmse_train, rmse_test, degradation
+
+def save_model(model,file_name):
+    # Saving model to an .pkl file
+    with open(file_name, 'wb') as file:
+        pickle.dump(model,file)
+
+def load_model(file_name):
+    with open(file_name, 'rb') as file:
+        return pickle.load(file)
+
 
 if __name__ == "__main__":
        df_cars = load_data("data/coches_usados_esp.csv",";")
        df_cars_clean = preprocess_data(df_cars)
-       # print(df_cars_clean)
-       print(df_cars_clean)
-       print(df_cars_clean[['months_old', 'power', 'kms', 'price']].isna().sum())
+
+       # Model
+       model, X_train, X_test, y_train, y_test = train_model(df_cars_clean)
+       evaluate_model(model, X_train, X_test, y_train, y_test)
+       save_model(model,"models/model_AdaBoost.pkl")
+       print("Model saved has models/model_AdaBoost.pkl")
+
